82fadbe065d51b5e6d0ad68b3c58783441e3308e
canbax <yusufsaidcanbaz@gmail.com>
2021-12-15 17:17:18 +0300

    format with prettier

--- demo.config.js
+++ demo.config.js
@@ -1,38 +1,38 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import livereload from 'rollup-plugin-livereload';
-import serve from 'rollup-plugin-serve';
-import pkg from './package.json';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import livereload from "rollup-plugin-livereload";
+import serve from "rollup-plugin-serve";
+import pkg from "./package.json";
 
 const liveReloadOpt = {
-  watch: ['src', 'demo', 'dist'],
+  watch: ["src", "demo", "dist"],
   delay: 300,
 };
 
 const serveOptions = {
   // Launch in browser (default: false)
   open: true,
-  openPage: '/demo/demo.html',
+  openPage: "/demo/demo.html",
   port: 8080,
-}
+};
 
 export default [
   // browser-friendly UMD build
   {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: {
-      name: 'cytoscape.js-visual-cues',
+      name: "cytoscape.js-visual-cues",
       file: pkg.browser,
-      format: 'umd',
-      exports: 'auto'
+      format: "umd",
+      exports: "auto",
     },
     plugins: [
-      nodeResolve(),   // so Rollup can find `ms`
-      commonjs(),  // so Rollup can convert `ms` to an ES module
+      nodeResolve(), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
       typescript(),
       serve(serveOptions),
-      livereload(liveReloadOpt)
-    ]
+      livereload(liveReloadOpt),
+    ],
   },
 ];
